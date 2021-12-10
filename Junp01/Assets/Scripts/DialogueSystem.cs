fd2375f6e67503a03abf17de92fb0b08c2fcad58
Andylin0930 <a0909123045@gmail.com>
2021-12-10 15:00:10 +0800

    協同系統01

---
+++ Junp01/Assets/Scripts/DialogueSystem.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections;
+
+public class DialogueSystem : MonoBehaviour
+{
+    [Header("對話系統"), Range(0, 1)]
+    public float interval = 0.2f;
+
+    private IEnumerator TypeEffect()
+    {
+        string test = "安安";
+       
+    } 
+}
